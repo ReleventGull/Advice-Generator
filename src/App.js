f12590e18e3fbe786c134fd8839d3b329d6a99c5
Jaron <jaronchretien@yahoo.com>
2023-05-04 18:09:29 -0500

    feat(Card): Renders the card for functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@
 
 const App = () => {
     return (
-        <h2>Hello World</h2>
+        <div className="mainBox">
+            <h4 className="adviceTitle">ADVICE #117</h4>
+            <p className="quote">"It is easy to sit up and take notice, what's
+                difficult is getting up and taking action."
+            </p>
+            <div className="bottomSection">
+                <img className="pauseImage" src="/images/pattern-divider-desktop.svg"/>
+            </div>
+      
+                <div className="circle">
+                    <img  src="/images/icon-dice.svg"/>
+                </div>
+    
+      
+            
+        </div>
     )
 }
 
